5cc6b52d9b2f13b874717723b7738ba355beb9ca
Donald Geddes <hi@donaldgeddes.ca>
2018-07-29 12:45:27 -0700

    Change base to https

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -1,6 +1,6 @@
 import axios from './config'
 
-const omdbBase = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&type=movie`
+const omdbBase = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&type=movie`
 
 export default {
   getMovieByTitle: title => axios.get(`${omdbBase}&t=${title}&tomatoes=true`).then(data => data.data),
